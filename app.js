fb48f883a7f229b70e494baa60f00c949ef28583
name <gramkracker88@gmail.com>
2020-09-02 19:35:26 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// node Value
+// textContent
+
+const item = document.getElementById('special');
+const value = item.firstChild.nodeValue;
+const easyValue = item.textContent;
+
+console.log(easyValue);
